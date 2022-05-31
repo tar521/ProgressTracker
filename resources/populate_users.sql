8d653e956aef6cfa787d8074c3aabe088d67d92a
Tristram Reed <tar123@txstate.edu>
2022-05-31 14:26:59 -0500

    file to populate users

---
+++ resources/populate_users.sql
@@ -0,0 +1,8 @@
+use progress_tracker;
+
+INSERT INTO users
+VALUES (null, 'default', 'default', '1234'),
+		(null, 'Sam', 'sam1', 'boats'),
+        (null, 'Dylan', 'dylan02', 'java'),
+        (null, 'Chocolate', 'coco', 'coffee');
+
