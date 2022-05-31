33ee8d08de9eb1dcdbbaeddb1e817db189617768
Tristram Reed <tar123@txstate.edu>
2022-05-31 11:21:08 -0500

    added driver

---
+++ src/com/cognixia/jump/progress/tracker/TrackerDriver.java
@@ -0,0 +1,28 @@
+package com.cognixia.jump.progress.tracker;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.cognixia.jump.progress.tracker.ConnectionManager;
+
+public class TrackerDriver {
+
+	public static void main(String[] args) {
+			
+			try {
+				
+				Connection conn = ConnectionManager.getConnection();
+				Statement stmt = conn.createStatement();
+				
+				
+				
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		
+	}
+}
+
