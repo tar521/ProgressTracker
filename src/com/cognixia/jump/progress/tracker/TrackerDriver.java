7c654f3acba38f46add51b12d7a0fb50561d320a
Tristram Reed <tar123@txstate.edu>
2022-05-31 15:50:34 -0500

    User Class Update

--- src/com/cognixia/jump/progress/tracker/TrackerDriver.java
+++ src/com/cognixia/jump/progress/tracker/TrackerDriver.java
@@ -3,12 +3,13 @@ package com.cognixia.jump.progress.tracker;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
 public class TrackerDriver {
-
-	private static Scanner sc;
+	
+	public static Scanner sc = new Scanner(System.in);
 	private static User user;
 	
 	public static void main(String[] args) {
@@ -24,8 +25,6 @@ public class TrackerDriver {
 				}
 				
 				System.out.println("\nConnection Success!\n");
-
-				sc = new Scanner(System.in);
 				
 				userMenu();
 				
@@ -42,20 +41,56 @@ public class TrackerDriver {
 	
 	public static void userMenu() {
 		System.out.println("Welcome to your TV Show Tracker!");
-		System.out.println("Please choose an option below:\n");
 		
-		System.out.println(" 1) View TV Show Tracker");
-//		System.out.println(" 2) View TV Shows not completed");
-//		System.out.println(" 3) View TV Shows in progress");
-//		System.out.println(" 4) View TV Show that are completed");
-		System.out.println(" 2) Select TV Show to add to list");
-		System.out.println(" 3) Update TV Show on list");
-		System.out.println(" 4) Remove TV Show from list");
-		System.out.println(" 5) Exit Application\n");
-		
-		
-		
-		
+		while (true) {
+			System.out.println("Please choose an option below:\n");
+			
+			System.out.println(" 1) View TV Show Tracker");
+	//		System.out.println(" 2) View TV Shows not completed");
+	//		System.out.println(" 3) View TV Shows in progress");
+	//		System.out.println(" 4) View TV Show that are completed");
+			System.out.println(" 2) Select TV Show to add to list");
+			System.out.println(" 3) Update TV Show on list");
+			System.out.println(" 4) Remove TV Show from list");
+			System.out.println(" 5) Exit Application\n");
+			try {
+				
+				int option = sc.nextInt();
+				sc.nextLine();
+				
+				switch (option) {
+					case 1:
+						// VIEW USER TRACKER
+						System.out.println("UNIMPLEMENTED OPTION");
+						break;
+					case 2:
+						// ADD SHOW TO TRACKER
+						System.out.println("UNIMPLEMENTED OPTION");
+						break;
+					case 3:
+						// UPDATE SHOW ON TRACKER - STATUS
+						System.out.println("UNIMPLEMENTED OPTION");
+						break;
+					case 4:
+						// REMOVE SHOW FROM TRACKER
+						System.out.println("UNIMPLEMENTED OPTION");
+						break;
+					case 5:
+						// EXIT TRACKER APP
+						System.out.println("Thanks for using the TV Show Tracker!");
+						break;
+					default:
+						System.out.println("Invalid Input - Please enter a listed option");
+				}
+			
+				if (option == 5) {
+					break;
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid input - please input an integer option");
+				sc.nextLine();
+			}
+		}
 	}
 }
 
