5ecb9ea5eb5bb27ceb1c9da288287002f3037e27
Tristram Reed <tar123@txstate.edu>
2022-06-02 11:29:38 -0500

    Edit toString and expected outputs in test

--- src/com/cognixia/jump/dao/TVShow.java
+++ src/com/cognixia/jump/dao/TVShow.java
@@ -44,7 +44,7 @@ public class TVShow {
 	}
 	@Override
 	public String toString() {
-		return "Title: " + title + ", ID: " + id + ", Length: " + length + ", Rating: " + rating;
+		return "ID: " + id + ", Title: " + title + ", Length: " + length + ", Rating: " + rating;
 	}
 	
 	
